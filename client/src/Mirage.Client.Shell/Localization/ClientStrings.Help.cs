84ace436472ff498238d42e0fcd14caa3c26ac80
Matt N <matt.nwachukwu@gmail.com>
2026-09-21 22:40:29 -0400

    Added missing commands to help

--- client/src/Mirage.Client.Shell/Localization/ClientStrings.Help.cs
+++ client/src/Mirage.Client.Shell/Localization/ClientStrings.Help.cs
@@ -47,6 +47,10 @@ public static partial class ClientStrings
     public const string HelpText_Cmd_Ban = nameof(HelpText_Cmd_Ban);
     public const string HelpText_Cmd_HwBan = nameof(HelpText_Cmd_HwBan);
     public const string HelpText_Cmd_HwUnban = nameof(HelpText_Cmd_HwUnban);
+    public const string HelpText_Cmd_Unban = nameof(HelpText_Cmd_Unban);
+    public const string HelpText_Cmd_Unkick = nameof(HelpText_Cmd_Unkick);
+    public const string HelpText_Cmd_Unmute = nameof(HelpText_Cmd_Unmute);
+    public const string HelpText_Cmd_Moderation = nameof(HelpText_Cmd_Moderation);
     public const string HelpText_Cmd_Mute = nameof(HelpText_Cmd_Mute);
     public const string HelpText_Cmd_Loc = nameof(HelpText_Cmd_Loc);
     public const string HelpText_Cmd_Debug = nameof(HelpText_Cmd_Debug);
